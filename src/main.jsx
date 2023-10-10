8753c7de3b5af9f45a2455e9a6188ae9adc124f4
Yull Timoteo <yull.timoteo.z@uni.pe>
2023-10-09 23:54:29 -0500

    Adding reset stylization

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { global, reset } from "./assets/global";
+import { Global } from "@emotion/react";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Global styles={reset} />
+    <Global styles={global} />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
