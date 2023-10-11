d47f0872f3b93c514e9b245726be914d33de5b80
Yull Timoteo <yull.timoteo.z@uni.pe>
2023-10-11 14:45:55 -0500

    Component creation and basic styling of PanelHome

---
+++ src/components/PanelHome/PanelHome.jsx
@@ -0,0 +1,61 @@
+import styled from "@emotion/styled";
+import urlImg from "../../assets/images/Panel-Home.svg";
+
+const ContainerPrimary = styled.div`
+  aspect-ratio: 2.4 / 1;
+  width: 100%;
+  background: url(${urlImg}) no-repeat center center;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 4rem;
+  padding: 3.75rem;
+`;
+
+const ContainerContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  h1 {
+    //styleName: Headline2;
+    font-family: Montserrat;
+    font-size: 64px;
+    font-weight: 300;
+    line-height: 88px;
+    letter-spacing: -0.5px;
+    text-align: center;
+    color: #373737;
+    @media (max-width: 1000px) {
+      font-size: 32px;
+      font-weight: 500;
+      line-height: 44px;
+    }
+  }
+  p {
+    //styleName: Headline5;
+    font-family: Montserrat;
+    font-size: 24px;
+    font-weight: 400;
+    line-height: 32px;
+    letter-spacing: 0px;
+    text-align: center;
+    color: #616161;
+    @media (max-width: 1000px) {
+      font-size: 12px;
+    }
+  }
+`;
+
+export default function PanelHome() {
+  return (
+    <ContainerPrimary>
+      <ContainerContent>
+        <h1>Meet your new Home</h1>
+        <p>The easiest way to find where you belong</p>
+      </ContainerContent>
+      <p>Search</p>
+    </ContainerPrimary>
+  );
+}
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,11 @@
 import NavBar from "../components/Menu/NavBar";
+import PanelHome from "../components/PanelHome/PanelHome";
 
 function LandingPage() {
   return (
     <>
       <NavBar haveToken={true} role="tenant" />
+      <PanelHome />
     </>
   );
 }
