fa23fae50e3420daadbd72b0a710ac6d51c9004d
Katya Anco <ladygetya@gmail.com>
2023-10-11 00:34:11 -0500

    change branch

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,31 @@
-import { Menu } from "../components/Menu/Menu";
-
+// import { Card } from "../components/Card/Card";
+import CarrouselSection from "../components/CarrouselSection";
+import HeroSection from "../components/HeroSection";
+import InfoSection from "../components/InfoSection";
+import TeamSection from "../components/TeamSection";
+import Footer from "../ui/Footer";
+import Header from "../ui/Header";
 function LandingPage() {
-  return <><Menu></Menu></>;
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <CarrouselSection />
+      <InfoSection />
+      <TeamSection />
+      <Footer />
+      {/* <div className="card__container">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div> */}
+    </>
+  );
 }
 
 export default LandingPage;
+
