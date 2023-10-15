2ea498f7a588eca51df9269e7150cecfa4849433
Yull Timoteo <yull.timoteo.z@uni.pe>
2023-10-14 21:10:57 -0500

    Creating test data in members.js

--- src/data/members.js
+++ src/data/members.js
@@ -30,3 +30,232 @@ export const members = [
     url: "https://avatars.githubusercontent.com/u/91983152?v=4",
   },
 ];
+
+export const withoutUser = null;
+
+export const customer = {
+  name: "Yull Timoteo",
+  email: "yull@mail.com",
+  phone: "955258139",
+  role: "customer",
+  favoriteProperties: [1, 2, 3],
+  propertiesContacted: [1, 2, 3],
+};
+export const tenant = {
+  name: "Yull Timoteo",
+  email: "yull@mail.com",
+  phone: "955258139",
+  role: "tenant",
+  favoriteProperties: [1, 2, 3],
+  propertiesContacted: [1, 2, 3],
+};
+
+export const property1 = {
+  id: 1,
+  address: "86872 Jacob Gateway, Durganport, WV 48044",
+  price: 3000,
+  rent: 2000,
+  typeSale: "sale",
+  typeProperty: "Apartament",
+  bedrooms: 2,
+  bathromms: 2,
+  area: 100.0,
+  about:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni vel atque quo? Nobis quibusdam libero culpa nisi sed non, eius ad soluta at amet doloremque atque est in fuga qui.",
+  photos: [
+    "https://picsum.photos/id/1026/640",
+    "https://picsum.photos/id/1/640",
+    "https://picsum.photos/id/1019/640",
+  ],
+  // role: "tenant",
+  active: false,
+  latitude: -12.125020769904085,
+  longitude: -77.01900139753451,
+};
+export const property2 = {
+  id: 2,
+  address: "86872 Jacob Gateway, Durganport, WV 48044",
+  price: 3000,
+  rent: 2000,
+  typeSale: "sale",
+  typeProperty: "Apartament",
+  bedrooms: 2,
+  bathromms: 2,
+  area: 100.0,
+  about:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni vel atque quo? Nobis quibusdam libero culpa nisi sed non, eius ad soluta at amet doloremque atque est in fuga qui.",
+  photos: [
+    "https://picsum.photos/id/1026/640",
+    "https://picsum.photos/id/1/640",
+    "https://picsum.photos/id/1019/640",
+  ],
+  // role: "tenant",
+  active: false,
+  latitude: -12.125020769904085,
+  longitude: -77.01900139753451,
+};
+export const property3 = {
+  id: 3,
+  address: "86872 Jacob Gateway, Durganport, WV 48044",
+  price: 3000,
+  rent: 2000,
+  typeSale: "sale",
+  typeProperty: "Apartament",
+  bedrooms: 2,
+  bathromms: 2,
+  area: 100.0,
+  about:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni vel atque quo? Nobis quibusdam libero culpa nisi sed non, eius ad soluta at amet doloremque atque est in fuga qui.",
+  photos: [
+    "https://picsum.photos/id/1026/640",
+    "https://picsum.photos/id/1/640",
+    "https://picsum.photos/id/1019/640",
+  ],
+  // role: "tenant",
+  active: false,
+  latitude: -12.125020769904085,
+  longitude: -77.01900139753451,
+};
+export const property4 = {
+  id: 4,
+  address: "86872 Jacob Gateway, Durganport, WV 48044",
+  price: 3000,
+  rent: 2000,
+  typeSale: "sale",
+  typeProperty: "Apartament",
+  bedrooms: 2,
+  bathromms: 2,
+  area: 100.0,
+  about:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni vel atque quo? Nobis quibusdam libero culpa nisi sed non, eius ad soluta at amet doloremque atque est in fuga qui.",
+  photos: [
+    "https://picsum.photos/id/1026/640",
+    "https://picsum.photos/id/1/640",
+    "https://picsum.photos/id/1019/640",
+  ],
+  // role: "tenant",
+  active: false,
+  latitude: -12.125020769904085,
+  longitude: -77.01900139753451,
+};
+
+export const rental1 = {
+  id: 1,
+  address: "86872 Jacob Gateway, Durganport, WV 48044",
+  price: 3000,
+  rent: 2000,
+  typeSale: "sale",
+  typeProperty: "Apartament",
+  maintanance: 100,
+  bedrooms: 2,
+  bathromms: 2,
+  area: 100.0,
+  petsAllowd: true,
+  about:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni vel atque quo? Nobis quibusdam libero culpa nisi sed non, eius ad soluta at amet doloremque atque est in fuga qui.",
+  photos: [
+    "https://picsum.photos/id/1026/640",
+    "https://picsum.photos/id/1/640",
+    "https://picsum.photos/id/1019/640",
+  ],
+  // role: "tenant",
+  active: false,
+  latitude: -12.125020769904085,
+  longitude: -77.01900139753451,
+};
+export const rental2 = {
+  id: 2,
+  address: "86872 Jacob Gateway, Durganport, WV 48044",
+  price: 3000,
+  rent: 2000,
+  typeSale: "sale",
+  typeProperty: "Apartament",
+  maintanance: 100,
+  bedrooms: 2,
+  bathromms: 2,
+  area: 100.0,
+  petsAllowd: false,
+  about:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni vel atque quo? Nobis quibusdam libero culpa nisi sed non, eius ad soluta at amet doloremque atque est in fuga qui.",
+  photos: [
+    "https://picsum.photos/id/1026/640",
+    "https://picsum.photos/id/1/640",
+    "https://picsum.photos/id/1019/640",
+  ],
+  // role: "tenant",
+  active: false,
+  latitude: -12.125020769904085,
+  longitude: -77.01900139753451,
+};
+export const rental3 = {
+  id: 3,
+  address: "86872 Jacob Gateway, Durganport, WV 48044",
+  price: 3000,
+  rent: 2000,
+  typeSale: "sale",
+  typeProperty: "Apartament",
+  maintanance: 100,
+  bedrooms: 2,
+  bathromms: 2,
+  area: 100.0,
+  petsAllowd: false,
+  about:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni vel atque quo? Nobis quibusdam libero culpa nisi sed non, eius ad soluta at amet doloremque atque est in fuga qui.",
+  photos: [
+    "https://picsum.photos/id/1026/640",
+    "https://picsum.photos/id/1/640",
+    "https://picsum.photos/id/1019/640",
+  ],
+  // role: "tenant",
+  active: true,
+  latitude: -12.125020769904085,
+  longitude: -77.01900139753451,
+};
+export const rental4 = {
+  id: 3,
+  address: "86872 Jacob Gateway, Durganport, WV 48044",
+  price: 3000,
+  rent: 2000,
+  typeSale: "sale",
+  typeProperty: "Apartament",
+  maintanance: 100,
+  bedrooms: 2,
+  bathromms: 2,
+  area: 100.0,
+  petsAllowd: true,
+  about:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni vel atque quo? Nobis quibusdam libero culpa nisi sed non, eius ad soluta at amet doloremque atque est in fuga qui.",
+  photos: [
+    "https://picsum.photos/id/1026/640",
+    "https://picsum.photos/id/1/640",
+    "https://picsum.photos/id/1019/640",
+  ],
+  // role: "tenant",
+  active: true,
+  latitude: -12.125020769904085,
+  longitude: -77.01900139753451,
+};
+export const rental5 = {
+  id: 3,
+  address: "86872 Jacob Gateway, Durganport, WV 48044",
+  price: 3000,
+  rent: 2000,
+  typeSale: "sale",
+  typeProperty: "Apartament",
+  maintanance: 100,
+  bedrooms: 2,
+  bathromms: 2,
+  area: 100.0,
+  petsAllowd: false,
+  about:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni vel atque quo? Nobis quibusdam libero culpa nisi sed non, eius ad soluta at amet doloremque atque est in fuga qui.",
+  photos: [
+    "https://picsum.photos/id/1026/640",
+    "https://picsum.photos/id/1/640",
+    "https://picsum.photos/id/1019/640",
+  ],
+  // role: "tenant",
+  active: true,
+  latitude: -12.125020769904085,
+  longitude: -77.01900139753451,
+};
