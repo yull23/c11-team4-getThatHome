aec2f6d2686ce85ef85c4b3bbc3704818da3c50d
Yull Timoteo <yull.timoteo.z@uni.pe>
2023-10-23 13:09:45 -0500

    Adding handleDelete to delete landlord property

--- src/services/properties-services.js
+++ src/services/properties-services.js
@@ -41,3 +41,9 @@ export async function propertiesContacted() {
   const showProperties = properties.map((property) => propertyView(property));
   return showProperties;
 }
+
+export function deleteProperty(id) {
+  return apiFetch(`properties/${id}`, { method: "DELETE" }).then(
+    (response) => response
+  );
+}
